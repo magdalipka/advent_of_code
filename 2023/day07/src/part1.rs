1801bf98985092e38a4e14bb3fe28af0a3bb531f
Magda Lipka <dev@magdalipka.com>
2023-12-13 12:06:33 +0100

    day: 2023-07; part 1

---
+++ 2023/day07/src/part1.rs
@@ -0,0 +1,93 @@
+use itertools::Itertools;
+use std::{cmp::Ordering, iter::zip};
+
+fn card_type(v: char) -> u32 {
+    return match v {
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => 11,
+        'T' => 10,
+        x => x.to_digit(10).unwrap(),
+    };
+}
+
+fn hand_type(v: &String) -> u32 {
+    let chars = v.chars().counts();
+
+    if chars.len() == 1 {
+        // five of a kind
+        return 7;
+    };
+    if chars.len() == 2 && chars.values().contains(&4) {
+        // four of a kind
+        return 6;
+    }
+    if chars.len() == 2 && chars.values().contains(&3) {
+        // full house
+        return 5;
+    }
+    if chars.len() == 3 && chars.values().contains(&3) {
+        // three of a kind
+        return 4;
+    }
+    if chars.len() == 3 {
+        // three of a kind
+        return 3;
+    }
+    if chars.len() == 4 {
+        // three of a kind
+        return 2;
+    }
+    return 1;
+}
+
+fn compare(a: &String, b: &String) -> Ordering {
+    if hand_type(a) > hand_type(b) {
+        return Ordering::Greater;
+    };
+    if hand_type(b) > hand_type(a) {
+        return Ordering::Less;
+    };
+    for (va, vb) in zip(a.chars(), b.chars()) {
+        if card_type(va) > card_type(vb) {
+            return Ordering::Greater;
+        };
+        if card_type(vb) > card_type(va) {
+            return Ordering::Less;
+        };
+    }
+    return Ordering::Equal;
+}
+
+pub fn process(input: &str) -> u32 {
+    let mut bids = input
+        .lines()
+        .map(|line| line.split_once(" ").unwrap())
+        .map(|bid| (bid.0.to_owned(), bid.1.parse::<u32>().unwrap()))
+        .collect::<Vec<_>>();
+    bids.sort_by(|a, b| compare(&a.0, &b.0));
+
+    return bids
+        .iter()
+        .enumerate()
+        .inspect(|v| println!("v: {:?}", v))
+        .fold(0, |acc, (index, value)| {
+            acc + ((index as u32 + 1) * value.1)
+        });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+        assert_eq!(process(input), 6440);
+    }
+}
